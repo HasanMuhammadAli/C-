77a06b2a5425dbc63fc46e5c7a1e1aa6d8cada9f
Muhammad Ali Hasan <mail.hasan.612@gmail.com>
2024-03-07 22:55:43 +0530

    STL-vector added

--- STL-vector.cpp
+++ STL-vector.cpp
@@ -1,8 +1,23 @@
 #include<vector>
-
+#include<iostream>
 using namespace std;
 
 int main()
 {
+    vector<int> a1;
+    int k;
+    for(int i=0;i<10;i++)
+    {
+       // cin>>k;
+        a1.push_back(i);
+    }
+    a1.push_back(10);
+    a1.push_back(20);
+     for (auto it = a1.begin(); it != a1.end(); it++) 
+        cout << *it << " "; 
+    cout<<a1.size();
+    cout<<a1.max_size();
+    
+
     return 1;
 }
