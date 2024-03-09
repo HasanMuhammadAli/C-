ff93511d3d38b731346a6c033dbe674e838ed506
Muhammad Ali Hasan <mail.hasan.612@gmail.com>
2024-03-09 07:17:31 +0530

    Sizeof for 4 datatypes added

--- Sizeof_fn.cpp
+++ Sizeof_fn.cpp
@@ -3,5 +3,8 @@ using namespace std;
 
 int main(){
     cout<<"No. of Bytes taken up by int:"<<sizeof(int)<<endl;
+    cout<<"No. of Bytes taken up by char:"<<sizeof(char)<<endl;
+    cout<<"No. of Bytes taken up by float:"<<sizeof(float)<<endl;
+    cout<<"No. of Bytes taken up by double:"<<sizeof(double)<<endl;
     return 0;
 }
